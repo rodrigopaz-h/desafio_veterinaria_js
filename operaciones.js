10bf15b3173205b2c6f700b1f810c259936f3abe
Rodrigo Paz <rodrigo.pazhernandez@gmail.com>
2024-08-17 01:27:33 -0400

    first commit

---
+++ operaciones.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+function registrar(nombre, edad, animal, color, enfermedad) {
+    const data = fs.readFileSync('citas.json', 'utf8');
+    const citas = JSON.parse(data);
+
+    const nuevaCita = { nombre, edad, animal, color, enfermedad };
+    citas.push(nuevaCita);
+
+    fs.writeFileSync('citas.json', JSON.stringify(citas, null, 2));
+    console.log('Cita registrada con éxito.');
+}
+
+function leer() {
+    const data = fs.readFileSync('citas.json', 'utf8');
+    const citas = JSON.parse(data);
+    console.log(citas);
+}
+
+module.exports = { registrar, leer };
